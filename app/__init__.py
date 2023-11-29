8e3f0ffd16c18838036385862d7b4f4213039783
Yehia <yehialdallal@gmail.com>
2023-11-29 21:44:30 +0200

    new blueprint added

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ def create_app(config_class=Config):
 
     # Register blueprints here
 
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
